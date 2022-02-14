d4b9eaf266a010630d5d178212f232a9dd98c152
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2022-02-14 15:45:47 -0600

    Text Edit Etc...

--- app/src/main/java/org/OperatorFoundation/MoonbounceAndroidKotlin/MBAKVpnService.kt
+++ app/src/main/java/org/OperatorFoundation/MoonbounceAndroidKotlin/MBAKVpnService.kt
@@ -1,136 +1,237 @@
 package org.OperatorFoundation.MoonbounceAndroidKotlin
 
 import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
 import android.content.Intent
 import android.net.VpnService
 import android.os.ParcelFileDescriptor
-import android.widget.Button
-import android.widget.TextView
-import java.io.FileInputStream
-import java.io.FileOutputStream
+import java.net.DatagramSocket
 import java.net.InetSocketAddress
-import java.nio.ByteBuffer
-import java.nio.channels.DatagramChannel
+
 
 class MBAKVpnService: VpnService() {
     private var mThread: Thread? = null
     private var mInterface: ParcelFileDescriptor? = null
 
-    // configure a builder for the interface.
     private var builder: Builder = Builder()
-    val transportServerIP = ""
-    val localHost = "127.0.0.1"
-    val replicantServerPort = 2277
-    val shadowSocksServerPort = 2345
+    var transportServerIP = ""
+    var transportServerPort = 2277
     val dnsServerIP = "8.8.8.8"
     val route = "0.0.0.0"
-    var subnetMask = 32
+    val subnetMask = 32
 
-    // Services interface
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // Start a new session by creating a new thread.
-        mThread = Thread({
-            try {
-                println("Entered try block of onStartCommand function")
-                //a. Configure the TUN and get the interface.
-                prepareBuilder()
-
-                //b. Packets to be sent are queued in this input stream
-                val input = FileInputStream(
-                    mInterface!!.fileDescriptor
-                )
-                println("created input stream")
-                //b. Packets received need to be written to this output stream.
-                val output = FileOutputStream(
-                    mInterface!!.fileDescriptor
-                )
-                println("created output stream")
-
-                //c. The UDP channel can be used to pass/get ip package to/from server
-                val tunnel: DatagramChannel = DatagramChannel.open()
-                println("called DatagramChannel.open()")
-                // Connect to the server, localhost is used for demonstration only.
-                val socketAddress = InetSocketAddress(localHost, shadowSocksServerPort)
-                tunnel.connect(socketAddress)
-                println("called tunnel.connect()")
-
-                //d. Protect this socket, so package send by it will not be feedback to the vpn service.
-                protect(tunnel.socket())
-                println("called protect(), starting loop")
-
-                //e. Use a loop to pass packets.
-                while (true)
-                {
-                    // this is where we will add code to handle the packets.
-                    //get packet within
-                    //put packet to tunnel
-                    //get packet from tunnel
-                    //return packet with out
-                    //sleep is a must
-                    // Can replace input.available() with an int for testing purposes
-                    //val arrayBuffer = ByteArray(input.available())
-                    val arrayBuffer = ByteArray(10)
-                    val bytesRead = input.read(arrayBuffer)
-//                    if (bytesRead != input.available())
-//                    {
-//                        println("Bytes read not equal to bytes available")
-//                    }
-                    println("bytes read: $bytesRead")
-                    // This is intended just to receive the packet we just put in the tunnel
-                    val byteBuffer = ByteBuffer.allocate(arrayBuffer.size)
-                    // put and send may not be in the correct order...
-                    byteBuffer.put(arrayBuffer)
-                    tunnel.receive(byteBuffer)
-                    val sendBuffer = ByteBuffer.allocate(arrayBuffer.size)
-                    tunnel.send(sendBuffer, socketAddress)
-                    // this is where we will need to return the packet with output...
-                    //output.write()
-                    Thread.sleep(100)
-                }
-                println("exited loop")
-            }
-            catch (e: Exception)
-            {
-                // Catch any exception
-                e.printStackTrace()
-            }
-            finally
-            {
-                try
-                {
-                    if (mInterface != null)
-                    {
-                        mInterface!!.close()
-                        mInterface = null
-                    }
-                }
-                catch (e: Exception)
-                {
-                    println(e)
-                }
-            }
-        }, "MyVpnRunnable")
-
-        //start the service
-        mThread!!.start()
-        println("start() called")
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int
+    {
+        connect(intent)
         return START_STICKY
     }
 
-    override fun onDestroy() {
-        // TODO Auto-generated method stub
-        if (mThread != null) {
-            mThread!!.interrupt()
+    fun connect(intent: Intent?)
+    {
+
+        var maybeIP: String?
+        val maybePort: Int
+
+        if (intent != null)
+        {
+            maybeIP = intent.getStringExtra(SERVER_IP)
+            maybePort = intent.getIntExtra(SERVER_PORT, 0)
         }
+        else
+        {
+            print("MBAKVpnService intent was null")
+            return
+        }
+
+        if (maybeIP != null)
+        {
+            transportServerIP = maybeIP
+        }
+        else
+        {
+            print("Tried to connect without a valid IP")
+            return
+        }
+
+        if (maybePort != 0)
+        {
+            transportServerPort = maybePort
+        }
+        else
+        {
+            print("Tried to connect without a valid port")
+            return
+        }
+
+        try
+        {
+            var socket = DatagramSocket()
+            // Call VpnService.protect() to keep your app's tunnel socket outside of the system VPN and avoid a circular connection.
+
+            protect(socket)
+
+            // Call DatagramSocket.connect() to connect your app's tunnel socket to the VPN gateway.
+            val socketAddress = InetSocketAddress(transportServerIP, transportServerPort)
+            socket.connect(socketAddress)
+
+            // Call VpnService.Builder methods to configure a new local TUN interface on the device for VPN traffic.
+            prepareBuilder()
+
+        }
+        catch (error: Exception)
+        {
+            print("Error creating socket")
+            print(error)
+            return
+        }
+    }
+//    // Services interface
+//    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int
+//    {
+//        var maybeIP: String?
+//        val maybePort: Int
+//
+//        if (intent != null)
+//        {
+//            maybeIP = intent.getStringExtra(SERVER_IP)
+//            maybePort = intent.getIntExtra(SERVER_PORT, 0)
+//        }
+//        else
+//        {
+//            print("MBAKVpnService intent was null")
+//            return Service.START_NOT_STICKY
+//        }
+//
+//        if (maybeIP != null)
+//        {
+//            transportServerIP = maybeIP
+//        }
+//        else
+//        {
+//            print("Tried to connect without a valid IP")
+//            return Service.START_NOT_STICKY
+//        }
+//
+//        if (maybePort != 0)
+//        {
+//            transportServerPort = maybePort
+//        }
+//        else
+//        {
+//            print("Tried to connect without a valid port")
+//            return Service.START_NOT_STICKY
+//        }
+//
+//        // Start a new session by creating a new thread.
+//        mThread = Thread({
+//            try {
+//                println("Entered try block of onStartCommand function")
+//
+//                //a. Configure the TUN and get the interface.
+//                prepareBuilder()
+//
+//                //b. Packets to be sent are queued in this input stream
+//                val input = FileInputStream(
+//                    mInterface!!.fileDescriptor
+//                )
+//                println("created input stream")
+//                //b. Packets received need to be written to this output stream.
+//                val output = FileOutputStream(
+//                    mInterface!!.fileDescriptor
+//                )
+//                println("created output stream")
+//
+//                //c. The UDP channel can be used to pass/get ip package to/from server
+//                val tunnel: DatagramChannel = DatagramChannel.open()
+//                println("called DatagramChannel.open()")
+//                // Connect to the server, localhost is used for demonstration only.
+//                val socketAddress = InetSocketAddress(transportServerIP, transportServerPort)
+//                tunnel.connect(socketAddress)
+//                println("called tunnel.connect()")
+//
+//                //d. Protect this socket, so package send by it will not be feedback to the vpn service.
+//                protect(tunnel.socket())
+//                println("called protect(), starting loop")
+//
+//                //e. Use a loop to pass packets.
+//                while (true)
+//                {
+//                    // this is where we will add code to handle the packets.
+//                    //get packet within
+//                    //put packet to tunnel
+//                    //get packet from tunnel
+//                    //return packet with out
+//                    //sleep is a must
+//                    // Can replace input.available() with an int for testing purposes
+//                    //val arrayBuffer = ByteArray(input.available())
+//                    val arrayBuffer = ByteArray(10)
+//                    val bytesRead = input.read(arrayBuffer)
+////                    if (bytesRead != input.available())
+////                    {
+////                        println("Bytes read not equal to bytes available")
+////                    }
+//                    println("bytes read: $bytesRead")
+//                    // This is intended just to receive the packet we just put in the tunnel
+//                    val byteBuffer = ByteBuffer.allocate(arrayBuffer.size)
+//                    // put and send may not be in the correct order...
+//                    byteBuffer.put(arrayBuffer)
+//                    tunnel.receive(byteBuffer)
+//                    val sendBuffer = ByteBuffer.allocate(arrayBuffer.size)
+//                    tunnel.send(sendBuffer, socketAddress)
+//                    // this is where we will need to return the packet with output...
+//                    //output.write()
+//                    Thread.sleep(100)
+//                }
+//                println("exited loop")
+//            }
+//            catch (e: Exception)
+//            {
+//                // Catch any exception
+//                e.printStackTrace()
+//            }
+//            finally
+//            {
+//                try
+//                {
+//                    if (mInterface != null)
+//                    {
+//                        mInterface!!.close()
+//                        mInterface = null
+//                    }
+//                }
+//                catch (e: Exception)
+//                {
+//                    println(e)
+//                }
+//            }
+//        }, "MyVpnRunnable")
+//
+//        //start the service
+//        mThread!!.start()
+//        println("start() called")
+//        return START_STICKY
+//    }
+
+    override fun onDestroy()
+    {
+        // TODO Auto-generated method stub
+//        if (mThread != null) {
+//            mThread!!.interrupt()
+//        }
         super.onDestroy()
     }
 
-    fun prepareBuilder() {
-
+    fun prepareBuilder()
+    {
+        // Create a local TUN interface using predetermined addresses.
+        //  You typically use values returned from the VPN gateway during handshaking.
         mInterface = builder.setSession("MoonbounceAndroidKotlinVpnService")
-            .addAddress(localHost, subnetMask)
+            .addAddress(transportServerIP, subnetMask)
             .addDnsServer(dnsServerIP)
-            .addRoute(route, 0).establish()
+            .addRoute(route, 0)
+            .establish() // Call VpnService.Builder.establish() so that the system establishes the local TUN interface and begins routing traffic through the interface.
 
         println("finished setting up builder")
     }
--- app/src/main/java/org/OperatorFoundation/MoonbounceAndroidKotlin/MainActivity.kt
+++ app/src/main/java/org/OperatorFoundation/MoonbounceAndroidKotlin/MainActivity.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.net.VpnService
 import android.widget.Button
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
+import java.lang.Exception
+
+val SERVER_PORT = "ServerPort"
+val SERVER_IP = "ServerIP"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,33 +30,59 @@ class MainActivity : AppCompatActivity() {
     fun onClick(v: View?)
     {
         println("Clicked the connect button")
-        // Check to see if the user has already given permission
-        val vpnPrepareIntent = VpnService.prepare(applicationContext)
-        if (vpnPrepareIntent != null) // User has not yet given permission
+        val ipEditText = findViewById<EditText>(R.id.server_address)
+        val ipAddress = ipEditText.text.toString()
+
+        val portEditText = findViewById<EditText>(R.id.server_port)
+        val serverPortString = portEditText.text.toString()
+
+        if (ipAddress.isEmpty() || ipAddress.isBlank())
         {
-            var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-            { result ->
-                if (result.resultCode == Activity.RESULT_OK) // User has now given permission
+            print("Can't connect without a valid IP Address")
+        }
+        else
+        {
+            try
+            {
+                val serverPort = serverPortString.toInt()
+
+                // Call VpnService.prepare() to ask for permission (when needed).
+                val vpnPrepareIntent = VpnService.prepare(applicationContext)
+                if (vpnPrepareIntent != null) // User has not yet given permission
+                {
+                    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+                    { result ->
+                        if (result.resultCode == Activity.RESULT_OK) // User has now given permission
+                        {
+                            // Start the VPN Service
+                            val vpnServiceIntent = Intent(this, MBAKVpnService::class.java)
+                            vpnServiceIntent.putExtra(SERVER_IP, ipAddress)
+                            vpnServiceIntent.putExtra(SERVER_PORT, serverPort)
+                            startService(vpnServiceIntent)
+                        }
+                        else
+                        {
+                            // Once this is a library, we may want to return an error and/or the result code to the calling application.
+                            print("Attempted to get VPN Service permissions from the user, but failed.")
+                            print(result.resultCode)
+                        }
+                    }
+                    // Launches an activity to request permission
+                    resultLauncher.launch(vpnPrepareIntent)
+                }
+                else // User has already given permission VPN Service is already prepared
                 {
                     // Start the VPN Service
                     val vpnServiceIntent = Intent(this, MBAKVpnService::class.java)
+                    vpnServiceIntent.putExtra(SERVER_IP, ipAddress)
+                    vpnServiceIntent.putExtra(SERVER_PORT, serverPort)
                     startService(vpnServiceIntent)
                 }
-                else
-                {
-                    // Once this is a library, we may want to return an error and/or the result code to the calling application.
-                    print("Attempted to get VPN Service permissions from the user, but failed.")
-                    print(result.resultCode)
-                }
             }
-            // Launches an activity to request permission
-            resultLauncher.launch(vpnPrepareIntent)
-        }
-        else // User has already given permission VPN Service is already prepared
-        {
-            // Start the VPN Service
-            val vpnServiceIntent = Intent(this, MBAKVpnService::class.java)
-            startService(vpnServiceIntent)
+            catch (error: Exception)
+            {
+                print(error)
+            }
         }
     }
 }
