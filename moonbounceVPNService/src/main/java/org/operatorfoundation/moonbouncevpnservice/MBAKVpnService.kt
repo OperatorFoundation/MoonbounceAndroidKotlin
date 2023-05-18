66e9f4f489312fabd4ec195eb784335fb8277401
bragelbytes <joebragel@gmail.com>
2023-05-18 13:43:06 -0700

    Update MBAKVpnService.kt

--- moonbounceVPNService/src/main/java/org/operatorfoundation/moonbouncevpnservice/MBAKVpnService.kt
+++ moonbounceVPNService/src/main/java/org/operatorfoundation/moonbouncevpnservice/MBAKVpnService.kt
@@ -84,7 +84,13 @@ class MBAKVpnService : VpnService()
             (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
         manager.createNotificationChannel(chan)
         val notificationIntent = Intent(this, MBAKVpnService::class.java)
-        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        // Do we want the flag to be 0? If so, it crashes SDK 30+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            0
+        )
         val notification: Notification = Notification.Builder(this, notificationChannelId)
             .setSmallIcon(R.drawable.crecent_moon)
             .setContentTitle("VPN Service Channel")
