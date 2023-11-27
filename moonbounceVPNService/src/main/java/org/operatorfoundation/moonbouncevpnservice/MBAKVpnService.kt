e622eb11e9efb64ae130e8efe5a5de26edb9e6f5
bragelbytes <joebragel@gmail.com>
2023-11-27 14:33:09 -0800

    reverted library module back to compileSdk33

--- moonbounceVPNService/src/main/java/org/operatorfoundation/moonbouncevpnservice/MBAKVpnService.kt
+++ moonbounceVPNService/src/main/java/org/operatorfoundation/moonbouncevpnservice/MBAKVpnService.kt
@@ -6,7 +6,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED
 import android.graphics.Color
 import android.net.IpPrefix
 import android.net.VpnService
@@ -452,14 +451,7 @@ class MBAKVpnService : VpnService()
             .setContentIntent(pendingIntent)
             .build()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-        {
-            startForeground(foregroundID, notification, FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED)
-        }
-        else
-        {
-            startForeground(foregroundID, notification)
-        }
+        startForeground(foregroundID, notification)
     }
 
     fun broadcastStatus(action: String, statusDescription: String, status: Boolean)
